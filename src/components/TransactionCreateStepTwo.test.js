958b8c3d584e341fe67c1b3850dd41fde9e07a8a
JVPolidoro <joao.polidoro@hexalab.com.br>
2022-02-05 13:50:55 -0300

    Learing frontend tests

---
+++ src/components/TransactionCreateStepTwo.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import TransactionCreateStepTwoProps from "./TransactionCreateStepTwo";
+
+test("on initial render, the pay button is disabled", async () => {
+  render(<TransactionCreateStepTwoProps sender={{ id: "5" }} receiver={{ id: "5" }} />);
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
+});
+
+test("if an amount and note is entered, the pay button becomes enabled", async () => {
+  render(<TransactionCreateStepTwoProps sender={{ id: "5" }} receiver={{ id: "5" }} />);
+
+  userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
+  userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
+});
+
+test("if an amount and note is entered, the pay button becomes enabled", async () => {
+  render(<TransactionCreateStepTwoProps sender={{ id: "5" }} receiver={{ id: "5" }} />);
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
+
+  userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
+  userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
+});
